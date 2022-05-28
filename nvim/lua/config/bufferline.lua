b3f25ac5a409412a4abf8d7243080be06f44bd55
shawmon <pengxiaomeng@outlook.com>
2022-05-28 21:15:55 +0800

    feat: nvim 配置切换到 lua

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -4,7 +4,6 @@
 local variables = {
   ['opt:termguicolors@'] = true
 }
- 
 -- " These commands will navigate through buffers in order regardless of which mode you are using
 -- " e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
 -- nnoremap <silent>[b :BufferLineCycleNext<CR>

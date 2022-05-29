8bdc0f1a07ef3399f3ca65e4b31487bcaecd8340
shawmon <pengxiaomeng@outlook.com>
2022-05-29 09:24:06 +0800

    feat: 修复tab报错

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -28,27 +28,52 @@ local startup = function()
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
+      -- ["<Tab>"] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_next_item()
+      --   elseif luasnip.expand_or_jumpable() then
+      --     luasnip.expand_or_jump()
+      --   elseif has_words_before() then
+      --     cmp.complete()
+      --   else
+      --     fallback()
+      --   end
+      -- end, { "i", "s" }),
+  
+      -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_prev_item()
+      --   elseif luasnip.jumpable(-1) then
+      --     luasnip.jump(-1)
+      --   else
+      --     fallback()
+      --   end
+      -- end, { "i", "s" }),
+
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
+           cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+           vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
         else
-          fallback()
+           fallback()
         end
-      end, { "i", "s" }),
-  
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
+     end, {
+        "i",
+        "s",
+     }),
+     ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
+           cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+           vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
         else
-          fallback()
+           fallback()
         end
-      end, { "i", "s" }),
+     end, {
+        "i",
+        "s",
+     }),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },

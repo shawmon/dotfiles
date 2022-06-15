5c58b94ceff3f5af8d42f5d2a44d5dd31a17bd8c
shawmon <pengxiaomeng@outlook.com>
2022-06-15 09:56:41 +0800

    comment key mappings

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -3,6 +3,51 @@
 return {
   'numToStr/Comment.nvim',
   config = function()
-    require('Comment').setup()
+    Core.map({
+      -- {
+      --   mode = 'n',
+      --   key = 'gc',
+      --   action = '<cmd>lua require("Comment.api").toggle_current_linewise()<cr>',
+      --   desc = 'toggle current linewise',
+      -- },
+      -- {
+      --   mode = 'n',
+      --   key = '<c-\\>',
+      --   action = '<cmd>lua require("Comment.api").toggle_current_blockwise()<cr>',
+      --   desc = 'toggle current blockwise',
+      -- },
+      {
+        mode = 'n',
+        key = 'gc',
+        action = '<cmd>lua require("Comment.api").call("toggle_linewise_op")<cr>g@',
+        desc = 'toggle linewise op',
+      },
+      {
+        mode = 'n',
+        key = 'gb',
+        action = '<cmd>lua require("Comment.api").call("toggle_blockwise_op")<cr>g@',
+        desc = 'toggle blockwise op',
+      },
+      -- {
+      --   mode = 'x',
+      --   key = '<c-/>',
+      --   action = '<esc><cmd>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<cr>',
+      --   desc = 'toggle linewise op in visual mode',
+      -- },
+      {
+        mode = 'x',
+        key = 'gc',
+        action = '<esc><cmd>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<cr>',
+        desc = 'toggle linewise op in visual mode',
+      },
+      {
+        mode = 'x',
+        key = 'gb',
+        action = '<esc><cmd>lua require("Comment.api").toggle_blockwise_op(vim.fn.visualmode())<cr>',
+        desc = 'toggle blockwise op in visual mode',
+      },
+    })
+
+    require('Comment').setup({})
   end,
 }
